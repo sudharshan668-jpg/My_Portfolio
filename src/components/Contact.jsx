59f10db542c6832ac303ab27e3bd6db355764f91
sudharshan668-jpg <sudharshan668@gmail.com>
2025-11-23 08:51:07 +0530

    Responsive Design

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { constants } from "./Constants";
+import { ArrowUpRightIcon } from "@heroicons/react/24/solid";
 
 export default function Contact() {
   const telephoneLink = "tel:" + constants.phone;
+  const customLink = "->";
   return (
     <section className="py-16 px-6 max-w-4xl mx-auto" id="contact">
       <h2 className="text-3xl font-bold mb-6">{constants.contactTitle}</h2>
@@ -14,7 +16,17 @@ export default function Contact() {
         </a>
       </p>
       <p className="text-lg">
-        <a href={constants.linkedinLink}>Go to my Linkedin</a>
+        <a
+          className="cursor-pointer hover:underline underline text-black dark:text-white inline-flex items-center gap-1"
+          href={constants.linkedinLink}
+        >
+          Go to my linkedin
+          <ArrowUpRightIcon
+            className="w-4 h-16 text-black dark:text-white"
+            titleClass="text-black dark:text-white"
+            textAnchor="end"
+          />
+        </a>
       </p>
     </section>
   );
